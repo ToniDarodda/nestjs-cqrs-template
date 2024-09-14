7271df419cfc79bbaf69ae6d5ff428ac031b62b9
Toni Da Rodda <toni.da.rodda.pro@gmail.com>
2024-09-14 18:35:11 +0200

    feat: fix injection problem with symbol service:

---
+++ src/infrastructure/database/migrations/1726326014560-PostRefactoring.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class PostRefactoring1726326014560 implements MigrationInterface {
+    name = 'PostRefactoring1726326014560'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "business" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE, "version" integer NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, CONSTRAINT "UQ_c6894e962b80bc10a694c0271e2" UNIQUE ("name"), CONSTRAINT "UQ_4cca40c3813d4b88a83edb459bc" UNIQUE ("email"), CONSTRAINT "PK_0bd850da8dafab992e2e9b058e5" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "business"`);
+    }
+
+}
